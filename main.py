ee32c5fc34cff195929266bdca11a57d9175b61a
Tmpecho <TMPEcho@gmail.com>
2024-01-28 15:38:15 +0100

    Added ability to specify grid size when running program with args

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
+import argparse
 import os
+import time
 
 from grid import Grid, GridUpdater
 from visualize import print_grid
@@ -7,9 +8,9 @@ from visualize import print_grid
 UPDATE_SPEED_SECONDS: float = 0.1
 
 
-def main() -> None:
+def main(grid_height: int, grid_width: int) -> None:
     generation_number: int = 0
-    grid: Grid = Grid(40, 60)
+    grid: Grid = Grid(grid_height, grid_width)
 
     while True:
         print_grid(grid, generation_number)
@@ -22,4 +23,9 @@ def main() -> None:
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(description='Set custom grid size.')
+    parser.add_argument('grid_height', type=int, nargs='?', default=40, help='Height of the grid')
+    parser.add_argument('grid_width', type=int, nargs='?', default=40, help='Width of the grid')
+    args = parser.parse_args()
+
+    main(args.grid_height, args.grid_width)
